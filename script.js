7816f932fe9233d8c7ff28bfceb96928e3472067
Piyush Garg <ipiyushgarg@outlook.com>
2025-01-06 18:13:58 +0530

    refactor: added sum of multiple numbers

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 class StringCalculator {
   add(numbers) {
     if (numbers?.trim() === "") return 0;
+
+    // Check if there's a custom delimiter
+    let delimiter = ",";
+    const numArray = numbers.split(new RegExp(`[${delimiter}]`));
+
+    // Return the sum of valid numbers
+    return numArray.reduce((sum, num) => sum + +num, 0);
   }
 }
 
--- script.test.js
+++ script.test.js
@@ -6,4 +6,12 @@ describe("StringCalculator", () => {
   test("should return 0 for an empty string", () => {
     expect(calc.add("")).toBe(0);
   });
+
+  test("should return the number for a single number string", () => {
+    expect(calc.add("1")).toBe(1);
+  });
+
+  test("should return the sum of two/multiple numbers separated by a comma", () => {
+    expect(calc.add("1,2")).toBe(3);
+  });
 });
